211f72ee89c5c4d1de1f388ff61f9b6e17e85c10
Karthick Sekar <karthicksekarss@gmail.com>
2020-08-25 22:00:41 +0530

    InitialSource

---
+++ CSharpBasics/Extension/IntExtention.cs
@@ -0,0 +1,20 @@
+﻿namespace CSharpBasics.Extension
+{
+    public static class IntExtention
+    {
+        public static string GetEmployeeCodeWithCompanyName(this string value, string append)
+        {
+            return $"{value}---{append}";
+        }
+
+        public static string GetService(this NeedsExtension needsExtension)
+        {
+            return "NeedsExtension";
+        }
+
+        public static string ToString(this string value)
+        {
+            return "override existing ToString()";
+        }
+    }
+}
---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CSharpBasics.Extension;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string EmpCode = "120780";
+            NeedsExtension obj = new NeedsExtension();
+            Console.WriteLine(EmpCode.GetEmployeeCodeWithCompanyName("Google"));
+            Console.WriteLine(obj.GetService());
+            Console.WriteLine("9677".ToString());
+            Console.Read();
+        }
+    }
+
+    public sealed class NeedsExtension
+    {
+    }
+   
+}
